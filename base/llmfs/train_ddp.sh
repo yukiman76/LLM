51c13d5237a3347db333cdc221806ba8286e01c4
jeff <jeffreyr@securitydept.com>
2024-10-18 13:05:44 -0400

    cleanup, DDP testing

--- base/llmfs/train_ddp.sh
+++ base/llmfs/train_ddp.sh
@@ -1,3 +1,6 @@
 export MASTER_ADDR='localhost'
-export MASTER_PORT='29400'
-torchrun --nnodes=1 --nproc_per_node=1 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=$MASTER_ADDR:29400 llfs_train_ddp.py
+export MASTER_PORT='12355'
+export OMP_NUM_THREADS='1'
+torchrun --nnodes=1 --nproc_per_node=3 --rdzv_id=100 \
+         --rdzv_backend=c10d --rdzv_endpoint=$MASTER_ADDR:29400 \
+         llfs_train_ddp.py
