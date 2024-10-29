94788461fba14395893427bf829917daaf2f660c
jeff <jeffreyr@securitydept.com>
2024-10-29 14:27:07 -0400

    h100

--- base/llmfs/llfs_config.py
+++ base/llmfs/llfs_config.py
@@ -34,7 +34,6 @@ def get_config():
         config['num_heads'] = 32  # Number of attention heads in transformer
         config['hidden_dim'] = 4096  # Size of feedforward layer
         config['num_layers'] = 32  # Number of transformer layers
-        config['max_seq_length'] = 512  # Maximum sequence length (context_length)
-        config['dropout'] = 0.1
+        config['max_seq_length'] = 2048  # Maximum sequence length (context_length)
 
     return config
