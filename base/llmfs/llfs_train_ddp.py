5b920da1f8628e9d97e1d87b2611c569e464b404
jeff <jeffreyr@securitydept.com>
2024-10-29 13:23:30 -0400

    h100

--- base/llmfs/llfs_train_ddp.py
+++ base/llmfs/llfs_train_ddp.py
@@ -33,8 +33,8 @@ def cleanup():
 
 
 def setup(rank, world_size):
-    # os.environ['MASTER_ADDR'] = 'localhost'
-    # os.environ['MASTER_PORT'] = '12355'
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12356'
     
     dist.init_process_group(backend="nccl", rank=rank, world_size=world_size)
 
