215ce1d0f0ab528c3e2f056fe97c8efe33dd504f
jeff <jeffreyr@securitydept.com>
2024-10-29 17:20:35 -0400

    v100 DDP

--- base/llmfs/llfs_config.py
+++ base/llmfs/llfs_config.py
@@ -17,7 +17,7 @@ def get_config():
         config['num_heads'] = 32 # Number of attention heads in transformer
         config['hidden_dim'] = 4096  # Size of feedforward layer
         config['num_layers'] = 32 # Number of transformer layers
-        config['max_seq_length'] = 2048 # Maximum sequence length (context_length)
+        config['max_seq_length'] = 512 # Maximum sequence length (context_length)
 
     if "4090" in sdevice_name:
         print("Using Nvidia 4090 GPU config")
