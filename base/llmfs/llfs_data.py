638d2a3a5c7ceed8c1e753802d8e3db3cd960c22
jeff <jeffreyr@securitydept.com>
2024-10-31 19:57:22 -0400

    llm train

--- base/llmfs/llfs_data.py
+++ base/llmfs/llfs_data.py
@@ -53,8 +53,8 @@ def create_dataloader(sdir, tokenizer, batch_size=4, max_length=256, stride=128,
     #     sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank, shuffle=False, 
     #                                  drop_last=False)
 
-    en = load_dataset("allenai/c4", "en", nrows=1000)
-
+    en = load_dataset("allenai/c4", "en", split="train", streaming=True)
+    en = en.take(1_000)
     # Concatenate both datasets
     concatenated = concatenate_datasets([books_ds, en]) 
     
